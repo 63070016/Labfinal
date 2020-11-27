3a34e4610af07992c584767df44056a1ee34220a
63070016 <74257768+63070016@users.noreply.github.com>
2020-11-27 10:37:19 +0700

    Update show.php

--- show.php
+++ show.php
@@ -43,7 +43,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td><center><?php echo $Result['Price'];?></center></td>
       <td><center><?php echo $Result['Amount'];?></center></td>
       <td><center><?php echo $Result['Total'];?></center></td>
-      <td><?php echo '<a href="delete.php?id='.$Result['ID'].'">
+      <td><?php echo '<a href="delete.php?id='.$Result['id'].'">
       Delete</a>'?></td>
       <td><center><a href="delete.php"><input type="submit" value="Delete"  class="btn-default"></a></center></td>
     </tr>
