dd72eb1c2d8f3c3ec162d8b7fe40e1beb94ab6d5
63070016 <74257768+63070016@users.noreply.github.com>
2020-11-27 10:17:01 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'tonklaxi.mysql.database.azure.com', 'tonklazuzu2@tonklaxi' , 'Kla007as', 'labitf', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
+}
+
+
+$id = $_GET['id'];
+
+
+$sql = "DELETE FROM guestbook WHERE id='$id'";
+
+
+if (mysqli_query($conn, $sql)) {
+    <button onclick ="location.href='index.html'"> </button> ;
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
